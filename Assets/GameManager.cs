5e42f564cc608e198463eded6956780d64d8c4ee
robo-4r <robo20206@gmail.com>
2026-07-06 17:31:03 +0900

    0706

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -17,8 +17,6 @@ public class GameConfig
     public float potionDropChance = 0.1f;
 }
 
-
-
 [System.Serializable]
 public class SaveData
 {
@@ -29,87 +27,55 @@ public class SaveData
 
     public int gem;
     public int level;
-
     public int gemUpgradeCost;
 
-    public float exp;
-    public float expToNextLevel;
+    public double exp;
+    public double expToNextLevel;
 }
-
-
 
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private Text moneyText;
+    [SerializeField] private Text upgradeCostText;
+    [SerializeField] private Text gemText;
+    [SerializeField] private Text gemUpgradeCostText;
+    [SerializeField] private Text levelText;
+    [SerializeField] private Text expText;
+    [SerializeField] private Text boostCostText;
 
-    public Text moneyText;
-    public Text upgradeCostText;
-    public Text gemText;
-    public Text gemUpgradeCostText;
-    public Text levelText;
-    public Text expText;
-    public Text boostCostText;
+    private double money = 0;
+    private double moneyPerSecond = 1;
+    private double upgradeCost = 10;
+    private double boostCost = 1000;
 
+    private int gem = 0;
 
+    private int gemUpgradeCost = 50;
 
-    public double money = 0;
-    public double moneyPerSecond = 1;
+    private int level = 1;
+    private double exp = 0;
+    private double expToNextLevel = 100;
 
-    public double upgradeCost = 10;
-
-    public double boostCost = 1000;
-
-
-
-    public int gem = 0;
-
-    int gemUpgradeCost = 50;
-
-
-
-    public int level = 1;
-
-    public float exp = 0;
-
-    public float expToNextLevel = 100;
-
-
-    public float GemUpgradeCost = 50;
-
-
-
-    public GameObject coinPanel;
-
-    public GameObject gemPanel;
-
-
+    [SerializeField] private GameObject coinPanel;
+    [SerializeField] private GameObject gemPanel;
+    [SerializeField] private GameObject resetConfirmPanel;
 
     GameConfig config = new GameConfig();
 
-
-
-    float potionTimer = 0;
-
-    bool isPotionActive = false;
-
-
-
+    private float potionTimer = 0;
+    private bool isPotionActive = false;
 
     string SavePath()
     {
         return Application.persistentDataPath + "/save.json";
     }
 
-
-
-
-
     string FormatNumber(double num)
     {
 
         if (num < 1000)
             return num.ToString("0");
-
 
         string[] suffix =
         {
@@ -122,11 +88,7 @@ public class GameManager : MonoBehaviour
             "Ce"
         };
 
-
-
         int index = 0;
-
-
 
         while (num >= 1000 && index < suffix.Length - 1)
         {
@@ -134,31 +96,18 @@ public class GameManager : MonoBehaviour
             index++;
         }
 
-
-
         return num.ToString("0.###") + suffix[index];
-
     }
-
-
-
-
 
 
     void Start()
     {
-
         DontDestroyOnLoad(gameObject);
-
 
         LoadGameData();
 
-
-
         TextAsset json =
         Resources.Load<TextAsset>("gameConfig");
-
-
 
         if (json != null)
         {
@@ -166,51 +115,32 @@ public class GameManager : MonoBehaviour
             JsonUtility.FromJson<GameConfig>(json.text);
         }
 
-
-
-
         if (coinPanel != null)
             coinPanel.SetActive(true);
-
-
 
         if (gemPanel != null)
             gemPanel.SetActive(false);
 
+        if (resetConfirmPanel != null)
+            resetConfirmPanel.SetActive(false);	
     }
-
-
-
-
-
 
 
     void Update()
     {
 
-
         float gemBonus =
         1 + gem * config.moneyMultiplierFromGems;
 
-
-
         float potionBonus =
         isPotionActive ? config.potionMoneyBoost : 1f;
-
-
-
 
         money += moneyPerSecond *
         gemBonus *
         potionBonus *
         Time.deltaTime;
 
-
-
         exp += (float)(moneyPerSecond * Time.deltaTime);
-
-
-
 
         if (exp >= expToNextLevel)
         {
@@ -218,44 +148,29 @@ public class GameManager : MonoBehaviour
         }
 
 
-
-
-
         if (moneyText != null)
             moneyText.text =
             "お金: " + FormatNumber(money);
-
-
 
         if (upgradeCostText != null)
             upgradeCostText.text =
             "強化: " + FormatNumber(upgradeCost);
 
-
-
         if (boostCostText != null)
             boostCostText.text =
             "倍率アップ: " + FormatNumber(boostCost);
-
-
 
         if (gemText != null)
             gemText.text =
             "ジェム: " + gem;
 
-
-
         if (gemUpgradeCostText != null)
             gemUpgradeCostText.text =
             "ジェム強化: " + gemUpgradeCost;
 
-
-
         if (levelText != null)
             levelText.text =
             "Lv: " + level;
-
-
 
         if (expText != null)
             expText.text =
@@ -264,45 +179,26 @@ public class GameManager : MonoBehaviour
             (int)expToNextLevel;
 
 
-
-
         if (isPotionActive)
         {
-
             potionTimer -= Time.deltaTime;
-
 
             if (potionTimer <= 0)
             {
                 isPotionActive = false;
             }
-
         }
-
     }
-
-
-
-
 
 
     void LevelUp()
     {
 
         exp -= expToNextLevel;
-
         level++;
-
         moneyPerSecond += 2;
-
         expToNextLevel *= 1.5f;
-
     }
-
-
-
-
-
 
 
     public void AddMoney()
@@ -311,56 +207,31 @@ public class GameManager : MonoBehaviour
     }
 
 
-
-
-
-
     public void Upgrade()
     {
-
         if (money >= upgradeCost)
         {
-
             money -= upgradeCost;
-
             moneyPerSecond += 1;
-
 
             upgradeCost *=
             config.upgradeCostMultiplier;
-
         }
 
     }
-
-
-
-
-
 
 
     public void BoostIncome()
     {
-
         if (money >= boostCost)
         {
-
             money -= boostCost;
-
 
             moneyPerSecond *= 1.5;
 
-
             boostCost *= 1.8;
-
         }
-
     }
-
-
-
-
-
 
 
     // ジェム強化
@@ -369,108 +240,50 @@ public class GameManager : MonoBehaviour
 
     public void GemUpgrade2()
     {
-
         if (gem >= gemUpgradeCost)
         {
-
             gem -= gemUpgradeCost;
-
-
             moneyPerSecond *= 1.5;
-
-
-            gemUpgradeCost *= 2;
-            
-            gem -= gemUpgradeCost;
-
-
+            gemUpgradeCost *= 2;           
         }
-
     }
-
-
-
-
-
-
 
     public void Rebirth()
     {
-
         if (money >= 500)
         {
-
             int getGem =
             Mathf.RoundToInt((float)(money / 100));
 
-
-
             gem += getGem;
 
-
-
             money = 0;
-
             moneyPerSecond = 1;
-
             upgradeCost = 10;
-
             boostCost = 1000;
-
         }
-
     }
-
-
-
-
-
 
 
     public void UsePotion()
     {
-
         isPotionActive = true;
 
         potionTimer =
         config.potionDuration;
-
     }
-
-
-
-
-
-
 
     public void OnClickChangeGemPanel()
     {
-
         coinPanel.SetActive(false);
-
         gemPanel.SetActive(true);
-
     }
-
-
-
-
-
-
 
     public void OnClickChangeCoinPanel()
     {
-
         coinPanel.SetActive(true);
-
         gemPanel.SetActive(false);
-
     }
-
-
-
-
-
 
 
     public void LoadGeimu()
@@ -479,66 +292,40 @@ public class GameManager : MonoBehaviour
     }
 
 
-
-
     public void LoadSample()
     {
         SceneManager.LoadScene("SampleScene");
     }
 
 
-
-
-
-
-
-
     public void SaveGame()
     {
-
         SaveData data =
         new SaveData();
-
-
 
         data.money = money;
 
         data.moneyPerSecond =
         moneyPerSecond;
 
-
         data.upgradeCost =
         upgradeCost;
-
 
         data.boostCost =
         boostCost;
 
-
-
         data.gem = gem;
-
         data.level = level;
-
-
 
         data.gemUpgradeCost =
         gemUpgradeCost;
 
-
-
         data.exp = exp;
-
         data.expToNextLevel =
         expToNextLevel;
 
-
-
-
         string json =
         JsonUtility.ToJson(data, true);
-
-
 
         File.WriteAllText(
         SavePath(),
@@ -547,85 +334,49 @@ public class GameManager : MonoBehaviour
     }
 
 
-
-
-
-
-
-
     public void LoadGameData()
     {
 
         if (!File.Exists(SavePath()))
             return;
 
-
-
         string json =
         File.ReadAllText(SavePath());
 
-
-
         SaveData data =
         JsonUtility.FromJson<SaveData>(json);
-
-
 
         money = data.money;
 
         moneyPerSecond =
         data.moneyPerSecond;
 
-
-
         upgradeCost =
         data.upgradeCost;
-
-
 
         boostCost =
         data.boostCost;
 
-
-
         gem =
         data.gem;
-
-
 
         level =
         data.level;
 
-
-
         gemUpgradeCost =
         data.gemUpgradeCost;
-
-
 
         exp =
         data.exp;
 
-
-
         expToNextLevel =
         data.expToNextLevel;
-
-
     }
-
-
-
-
-
 
     private void OnApplicationQuit()
     {
         SaveGame();
     }
-
-
-
 
     private void OnApplicationPause(bool pause)
     {
@@ -635,13 +386,52 @@ public class GameManager : MonoBehaviour
 
     }
 
-
-
-
-
     public void SaveButton()
     {
         SaveGame();
+    }
+
+    public void ResetGame()
+    {
+        money = 0;
+        moneyPerSecond = 1;
+
+        upgradeCost = 10;
+        boostCost = 1000;
+
+        gem = 0;
+        gemUpgradeCost = 50;
+
+        level = 1;
+        exp = 0;
+        expToNextLevel = 100;
+
+        isPotionActive = false;
+        potionTimer = 0;
+
+        if (File.Exists(SavePath()))
+        {
+         File.Delete(SavePath());
+        }
+
+        Debug.Log("セーブデータを削除しました");
+    }
+
+    public void ShowResetConfirm()
+    {
+        resetConfirmPanel.SetActive(true);
+        resetConfirmPanel.transform.SetAsLastSibling();
+    }
+
+    public void ConfirmReset()
+    {
+        ResetGame();
+        resetConfirmPanel.SetActive(false);
+    }
+
+    public void CancelReset()
+    {
+        resetConfirmPanel.SetActive(false);
     }
 
 }
